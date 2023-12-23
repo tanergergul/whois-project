e63c3464a92eda70c5ed1ba0575aafa68801202e
Taner Gergül <taner@gergul.dev>
2023-12-23 12:55:38 +0300

    IP Whois page coded.

--- index.js
+++ index.js
@@ -28,6 +28,40 @@ app.get("/", async (req,res) => {
     }
 })
 
+app.get("/ip", async (req,res) => {
+    try{
+        const response = await axios.get(API_URL + "ip");
+        var ip = response.data;
+        userIP = ip.addr;
+        ip.code = ip.code.toLowerCase();
+
+        res.render("ip.ejs",{
+            ip:ip
+        });
+    } catch (error){
+        console.log(error);
+        res.status(500);
+    }
+})
+
+app.post("/ip",async (req,res) => {
+    const whoisIP = req.body.whoisIP;
+
+    try{
+        const response = await axios.get(API_URL + "ip?addr=" + whoisIP);
+        var ip = response.data;
+        ip.code = ip.code.toLowerCase();
+        console.log("IP Whois: " + JSON.stringify(ip));
+
+        res.render("ip.ejs",{
+            ip:ip
+        });
+    } catch (error){
+        console.log(error);
+        res.status(500);
+    }
+})
+
 app.listen(port,() => {
     console.log(`Server is running on ${port}`);
 })
