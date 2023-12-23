89da7ee081540a5bebb0c73213164a6f0fce8925
Taner Gergül <taner@gergul.dev>
2023-12-23 11:48:13 +0300

    homepage coded

--- index.js
+++ index.js
@@ -6,14 +6,26 @@ import morgan from "morgan";
 const app = express();
 const port = 3000;
 var userIP = "";
+const API_URL = "https://scraper.run/"
 
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
-app.get("/", (req,res) => {
+app.get("/", async (req,res) => {
+    try{
+        const response = await axios.get(API_URL + "ip");
+        var ip = response.data;
+        userIP = ip.addr;
+        ip.code = ip.code.toLowerCase();
 
-    res.render("index.ejs");
+        res.render("index.ejs",{
+            ip:ip
+        });
+    } catch (error){
+        console.log(error);
+        res.status(500);
+    }
 })
 
 app.listen(port,() => {
