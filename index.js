ddd6621607669708bea5db5e7b0c4dda135ca8e3
Taner Gergül <taner@gergul.dev>
2023-12-25 00:11:03 +0300

    fix for wrong user ip on production

--- index.js
+++ index.js
@@ -8,15 +8,21 @@ const port = 3000;
 var userIP = "";
 const API_URL = "https://scraper.run/"
 
-app.use(morgan("dev"));
+app.use(morgan("common"));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 app.get("/", async (req,res) => {
+    userIP = "1.1.1.1";
+
+    if(req.socket.remoteAddress != "::1"){
+        userIP = req.socket.remoteAddress;
+    }
+
     try{
-        const response = await axios.get(API_URL + "ip");
+        const response = await axios.get(API_URL + "ip?addr=" + userIP);
         var ip = response.data;
-        userIP = ip.addr;
+        
         ip.code = ip.code.toLowerCase();
 
         res.render("index.ejs",{
@@ -29,10 +35,16 @@ app.get("/", async (req,res) => {
 })
 
 app.get("/ip", async (req,res) => {
+    userIP = "1.1.1.1";
+
+    if(req.socket.remoteAddress != "::1"){
+        userIP = req.socket.remoteAddress;
+    }
+
     try{
-        const response = await axios.get(API_URL + "ip");
+        const response = await axios.get(API_URL + "ip?addr=" + userIP);
         var ip = response.data;
-        userIP = ip.addr;
+
         ip.code = ip.code.toLowerCase();
 
         res.render("ip.ejs",{
